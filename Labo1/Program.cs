4ce1826f6740d80f50bebc0489d54568da223a07
Hexalthy219 <cyril.russe@hotmail.com>
2023-02-25 12:22:04 +0100

    Ajoutez des fichiers projet.

---
+++ Labo1/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using MaLibrairieForme;
+
+namespace Labo1 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Coordonnees test = new Coordonnees();
+            Console.WriteLine(test.ToString());
+        }
+    }
+}
---
+++ MaLibrairieForme/Coordonnees.cs
@@ -0,0 +1,25 @@
+﻿namespace MaLibrairieForme
+{
+    public class Coordonnees
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+
+        public Coordonnees(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public Coordonnees() : this(0, 0)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return "("+X+", "+Y+")";
+        }
+    }
+}
